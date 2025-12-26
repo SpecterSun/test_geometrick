490a6219cf9a81b131b5afe18483f3f73185f3d2
Alukard <137450843+SpecterSun@users.noreply.github.com>
2025-12-26 13:52:04 +0600

    Update test_square.py

--- test_square.py
+++ test_square.py
@@ -14,13 +14,20 @@ class SquareTestCase(unittest.TestCase):
         self.assertEqual(area(-5), 25)
 
     def test_area_string(self):
-        self.assertEqual(area("5"), 25)
+        with self.assertRaises(TypeError):
+            area("5")
 
     def test_area_list(self):
-        self.assertEqual(area([5]), 25)
+        with self.assertRaises(TypeError):
+            area([5])
 
     def test_area_none(self):
-        self.assertEqual(area(None), 0)
+        with self.assertRaises(TypeError):
+            area(None)
+
+    def test_area_boolean(self):
+        self.assertEqual(area(True), 1)
+        self.assertEqual(area(False), 0)
 
     def test_perimeter_normal(self):
         self.assertEqual(perimeter(5), 20)
@@ -29,16 +36,21 @@ class SquareTestCase(unittest.TestCase):
         self.assertEqual(perimeter(0), 0)
 
     def test_perimeter_negative(self):
-        self.assertEqual(perimeter(-3), 12)
+        self.assertEqual(perimeter(-3), -12)
 
     def test_perimeter_string(self):
-        self.assertEqual(perimeter("10"), 40)
+        self.assertEqual(perimeter("10"), "10101010")
 
     def test_perimeter_list(self):
-        self.assertEqual(perimeter([3]), 12)
+        self.assertEqual(perimeter([3]), [3, 3, 3, 3])
 
     def test_perimeter_none(self):
-        self.assertEqual(perimeter(None), 0)
+        with self.assertRaises(TypeError):
+            perimeter(None)
+
+    def test_perimeter_boolean(self):
+        self.assertEqual(perimeter(True), 4)
+        self.assertEqual(perimeter(False), 0)
 
 
 if __name__ == '__main__':
