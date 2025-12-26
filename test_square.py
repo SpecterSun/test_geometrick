c0c69a4a5d3653b7fc92eaf8dd741c25a22da967
Alukard <137450843+SpecterSun@users.noreply.github.com>
2025-12-26 13:25:09 +0600

    Update test_square.py

--- test_square.py
+++ test_square.py
@@ -10,11 +10,43 @@ class SquareTestCase(unittest.TestCase):
     def test_area_zero(self):
         self.assertEqual(area(0), 0)
 
+    def test_area_negative(self):
+        with self.assertRaises(ValueError):
+            area(-5)
+
+    def test_area_string(self):
+        with self.assertRaises(TypeError):
+            area("5")
+
+    def test_area_list(self):
+        with self.assertRaises(TypeError):
+            area([5])
+
+    def test_area_none(self):
+        with self.assertRaises(TypeError):
+            area(None)
+
     def test_perimeter_normal(self):
         self.assertEqual(perimeter(5), 20)
 
     def test_perimeter_zero(self):
         self.assertEqual(perimeter(0), 0)
+
+    def test_perimeter_negative(self):
+        with self.assertRaises(ValueError):
+            perimeter(-3)
+
+    def test_perimeter_string(self):
+        with self.assertRaises(TypeError):
+            perimeter("10")
+
+    def test_perimeter_list(self):
+        with self.assertRaises(TypeError):
+            perimeter([3])
+
+    def test_perimeter_none(self):
+        with self.assertRaises(TypeError):
+            perimeter(None)
 
 
 if __name__ == '__main__':
